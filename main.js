69dff49cb49142e58b4b1b4c345950916891658e
Pedro Barros Vivas <138531144+Pedrobvivas@users.noreply.github.com>
2024-02-27 13:19:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let toggleSwitch = document.querySelector('.toggle-switch');
 let body = document.body;
 let modoLabel = document.querySelector('.modo');
 
+
 toggleSwitch.addEventListener('click', () => {
     if (body.classList.contains('dark')) {
         body.classList.remove('dark');
