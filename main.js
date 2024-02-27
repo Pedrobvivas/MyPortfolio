05c460367fc90cd8d86aee2035372daa479d2463
Pedro Barros Vivas <estagiario.nti02@faesa.br>
2024-02-27 13:02:12 -0300

    Ajustes

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 let toggleSwitch = document.querySelector('.toggle-switch');
 let body = document.body;
+let modoLabel = document.querySelector('.modo');
 
 toggleSwitch.addEventListener('click', () => {
     if (body.classList.contains('dark')) {
         body.classList.remove('dark');
+        modoLabel.textContent = 'Modo Claro';
     } else {
         body.classList.add('dark');
+        modoLabel.textContent = 'Modo Escuro';
     }
 });
